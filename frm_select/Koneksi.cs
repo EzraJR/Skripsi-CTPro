6011a2e60a7ac0d82aa5b5321575ea06d1fd6b5e
EzraJR <ezra_janitra@yahoo.com>
2016-06-18 13:09:54 +0700

    versi1

---
+++ frm_select/Koneksi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace frm_select
+{
+   public class Koneksi
+    {
+     public  MySqlCommand cmd = new MySqlCommand();
+      public MySqlDataAdapter adp = new MySqlDataAdapter();
+       string konfigurasi = "server=localhost;User Id=root;database=db_penjualan";
+      public MySqlConnection connection;
+
+       public void openkoneksi()
+       {
+           try
+           {
+               connection = new MySqlConnection(konfigurasi);
+               connection.Open();
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show("Error: "+ex.Message);
+               throw;
+           }
+       }
+
+    }
+}
